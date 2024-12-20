024103a8f87852bf06d3cc5802cd383e3f93a91d
zerorequiem20 <harsh.bhavsar2020@gmail.com>
2024-12-20 12:31:16 +0000

    comment

--- src/main/java/adm_data_stripper.java
+++ src/main/java/adm_data_stripper.java
@@ -32,6 +32,7 @@ public class adm_data_stripper {
             }
         }
     }
+    //heiu3hew
 
     public String formatXML(Document doc) throws TransformerException {
         TransformerFactory transformerFactory = TransformerFactory.newInstance();
