dce14529b9da881df074be70ae929ee1cecb120b
zerorequiem20 <harsh.bhavsar2020@gmail.com>
2024-12-30 13:24:22 +0000

    added dockerfile and updated jenkins

--- src/main/java/adm_data_stripper.java
+++ src/main/java/adm_data_stripper.java
@@ -32,7 +32,7 @@ public class adm_data_stripper {
             }
         }
     }
-    //heiu3heweaaaefdewwefedrfrf
+    //heiu3heweaaaefdewwefedrfrfed
 
     public String formatXML(Document doc) throws TransformerException {
         TransformerFactory transformerFactory = TransformerFactory.newInstance();
